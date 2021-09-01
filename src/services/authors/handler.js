e34f3d3d4bfe25b58f1857e5d8a4ac58aadad561
attila <attilavigh.contact@gmail.com>
2021-09-01 13:10:04 +0100

    Indentation on author.js file. This file is outdated because the data was moved into MongoDB collection..

--- src/services/authors/handler.js
+++ src/services/authors/handler.js
@@ -2,73 +2,77 @@ import authors from "../../tools/fs-CRUD.js";
 import createError from "http-errors";
 import path from "path";
 
-const pathToFile = path.join(process.cwd(), "src/data/authors.json");
+const pathToFile = path.join( process.cwd(), "src/data/authors.json" );
 
-export const listAuthors = async (req, res, next) => {
+export const listAuthors = async ( req, res, next ) => {
     try
     {
-        const authorsList = await authors.read(pathToFile);
-        if (req.query && req.query.name)
+        const authorsList = await authors.read( pathToFile );
+        if ( req.query && req.query.name )
         {
-            const filteredAuthors = authorsList.filter(author =>
+            const filteredAuthors = authorsList.filter( author =>
                 author.name
                     .toLowerCase()
-                    .includes(req.query.name.toLowerCase())
+                    .includes( req.query.name.toLowerCase() )
             );
-            res.send(filteredAuthors);
+            res.send( filteredAuthors );
         }
         else
-            res.send(authorsList);
+            res.send( authorsList );
     }
-    catch (error)
+    catch ( error )
     {
-        next(createError(404, error.message));
+        next( createError( 404, error.message ) );
     }
 };
 
-export const singleAuthor = async (req, res, next) => {
-    console.log("req.params.id::: ", req.params.id);
+export const singleAuthor = async ( req, res, next ) => {
+    console.log( "req.params.id::: ", req.params.id );
     try
     {
-        const authorsList = await authors.single(req.params.id, pathToFile);
-        res.send(authorsList);
+        const authorsList = await authors.single( req.params.id, pathToFile );
+        res.send( authorsList );
     }
-    catch (error)
+    catch ( error )
     {
-        next(createError(500, error.message));
+        next( createError( 500, error.message ) );
     }
 };
 
-export const addAuthor = async (req, res, next) => {
-    console.log("req.params.id::: ", req.body);
+export const addAuthor = async ( req, res, next ) => {
+    console.log( "req.params.id::: ", req.body );
     try
     {
-        const newAuthor = await authors.new(req.body, pathToFile);
-        res.status(201).send(newAuthor);
+        const newAuthor = await authors.new( req.body, pathToFile );
+        res.status( 201 ).send( newAuthor );
     }
-    catch (error)
+    catch ( error )
     {
-        next(createError(500, error.message));
+        next( createError( 500, error.message ) );
     }
 };
 
-export const updateAuthor = async (req, res, next) => {
-    try {
-        const updatedAuthor = await authors.update(req.params.id, req.body, pathToFile);
-        res.status(204).send(updatedAuthor);
+export const updateAuthor = async ( req, res, next ) => {
+    try
+    {
+        const updatedAuthor = await authors.update( req.params.id, req.body, pathToFile );
+        res.status( 204 ).send( updatedAuthor );
     }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 500, error.message ) );
     }
 };
 
-export const deleteAuthor = async (req, res, next) => {
-    try {
-        await authors.delete( req.params.id, pathToFile);
-        res.status(204).send();
+export const deleteAuthor = async ( req, res, next ) => {
+    try
+    {
+        await authors.delete( req.params.id, pathToFile );
+        res.status( 204 ).send();
     }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 500, error.message ) );
     }
 };
 
